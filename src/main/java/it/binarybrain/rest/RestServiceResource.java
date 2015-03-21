4e3963c330ac2efed6d9476c5e00e7880a7e9c56
Gia90 <gia.90@hotmail.it>
2015-03-21 20:54:11 +0100

    Realizzato primo semplice servizio REST attivo sul path /merio

--- src/main/java/it/binarybrain/rest/RestServiceResource.java
+++ src/main/java/it/binarybrain/rest/RestServiceResource.java
@@ -2,16 +2,13 @@ package it.binarybrain.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-@Path("hello")
+@Path("merio")
 public class RestServiceResource {
 	
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     public String sayhello() {
-        return "hello";
+        return "I <3 U";
     }
 
 }
