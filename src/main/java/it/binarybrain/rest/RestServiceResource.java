1523ac18fbf87e05d4f32485a8cf3841b70cdcae
Gia90 <gia.90@hotmail.it>
2015-03-21 19:58:52 +0100

    Aggiunta del codice e delle configurazioni base per lo sviluppo del servizio RESTful

---
+++ src/main/java/it/binarybrain/rest/RestServiceResource.java
@@ -0,0 +1,17 @@
+package it.binarybrain.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("hello")
+public class RestServiceResource {
+	
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayhello() {
+        return "hello";
+    }
+
+}
